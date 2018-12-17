1af12aaa178d1d4aacd6f134021447d767e69d75
Dakae <39431876+Dakae@users.noreply.github.com>
2018-12-17 12:16:53 +0900

    Add files via upload

---
+++ 3moters.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(4,HIGH);
+digitalWrite(7,HIGH);
+digitalWrite(8,HIGH);
+
+analogWrite(5,255);
+analogWrite(6,255);
+analogWrite(9,255);
+
+}
